92e04ff1ba29beb289bdfc4a1bb70aa83a2c758b
José Manuel Hernández <jmhernandez@techtitute.com>
2021-05-07 15:53:31 +0100

    render dropdown fields and bind observable streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig } from '@ngx-formly/core';
+import { DataService } from './core/data.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent {
   form = new FormGroup({});
   model = {
     firstName: 'John',
-    age: 44
+    age: 44,
+    nationId: 2
   };
   fields: FormlyFieldConfig[] = [
     {
@@ -26,10 +28,21 @@ export class AppComponent {
       type: 'input',
       templateOptions: {
         type: 'number',
-        label: 'Age'
+        label: 'Age',
+        nationId: 2
+      }
+    },
+    {
+      key: 'nationId',
+      type: 'select',
+      templateOptions: {
+        label: 'Nation',
+        options: this.dataService.getNations()
       }
     }
   ];
+
+  constructor(private dataService: DataService){}
 
   onSubmit({ valid, value }) {
     console.log(value);
