0888ae29795b9bc3a76c278e2daa7b42fd462652
José Manuel Hernández <jmhernandez@techtitute.com>
2021-05-12 11:52:28 +0100

    fix css. implement multi-column layout forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,15 @@ import { startWith, switchMap, tap } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 
 export function PetNameValidator(c: FormControl): ValidationErrors {
-  return !c.value || /(Max)|(Rex)/.test(c.value) ? null : { petName : true };
+  return !c.value || /(Max)|(Rex)/.test(c.value) ? null : { petName: true };
 }
+
+const formlyRow = (fieldConfig: FormlyFieldConfig[]) => {
+  return {
+    fieldGroupClassName: 'display-flex',
+    fieldGroup: fieldConfig
+  };
+};
 
 @Component({
   selector: 'app-root',
@@ -19,7 +26,7 @@ export class AppComponent {
   constructor(
     private dataService: DataService,
     private translate: TranslateService
-  ){
+  ) {
     this.translate.use('de');
   }
 
@@ -30,65 +37,73 @@ export class AppComponent {
     age: 44,
     countryId: 2,
     cityId: 1,
-    ip: null
+    petName: null
   };
   fields: FormlyFieldConfig[] = [
     {
       key: 'id'
     },
-    {
-      key: 'firstName',
-      type: 'input',
-      templateOptions: {
-        label: 'First Name',
-        required: true
-      }
-    },
-    {
-      key: 'age',
-      type: 'input',
-      templateOptions: {
-        type: 'number',
-        label: 'Age',
-        min: 18
-      },
-      validation: {
-        messages: {
-          min: 'You have to be 18 or older.'
+    formlyRow([
+      {
+        key: 'firstName',
+        type: 'input',
+        className: 'flex-3',
+        templateOptions: {
+          label: 'First Name',
+          required: true
         }
-      }
-    },
-    {
-      key: 'countryId',
-      type: 'my-autocomplete',
-      templateOptions: {
-        label: 'Country',
-        options: this.dataService.getCountries()
-      }
-    },
-    {
-      key: 'cityId',
-      type: 'select',
-      templateOptions: {
-        label: 'City',
-        options: []
       },
-      expressionProperties: {
-        'templateOptions.disabled': (model) => !model.countryId,
-        'model.cityId': '!model.nationId ? null : model.cityId'
-      },
-      hideExpression: model => !model.countryId,
-      hooks: {
-        onInit: (field: FormlyFieldConfig) => {
-          field.templateOptions.options = field.form.get('countryId').valueChanges.pipe(
-            startWith(this.model.countryId),
-            tap(),
-            switchMap(countryId => this.dataService.getCities(countryId))
-          );
+      {
+        key: 'age',
+        type: 'input',
+        className: 'flex-1',
+        templateOptions: {
+          type: 'number',
+          label: 'Age',
+          min: 18
+        },
+        validation: {
+          messages: {
+            min: 'You have to be 18 or older.'
+          }
         }
-      }
-    },
-    {
+      },
+    ]),
+    formlyRow([
+      {
+        key: 'countryId',
+        type: 'my-autocomplete',
+        className: 'flex-3',
+        templateOptions: {
+          label: 'Country',
+          options: this.dataService.getCountries()
+        }
+      },
+      {
+        key: 'cityId',
+        type: 'select',
+        className: 'flex-3',
+        templateOptions: {
+          label: 'City',
+          options: []
+        },
+        expressionProperties: {
+          'templateOptions.disabled': (model) => !model.countryId,
+          'model.cityId': '!model.nationId ? null : model.cityId'
+        },
+        hideExpression: model => !model.countryId,
+        hooks: {
+          onInit: (field: FormlyFieldConfig) => {
+            field.templateOptions.options = field.form.get('countryId').valueChanges.pipe(
+              startWith(this.model.countryId),
+              tap(),
+              switchMap(countryId => this.dataService.getCities(countryId))
+            );
+          }
+        }
+      },
+    ]),
+    formlyRow([{
       key: 'petName',
       type: 'input',
       templateOptions: {
@@ -101,7 +116,7 @@ export class AppComponent {
           message: 'Pet name must be Max or Rex.'
         }
       }
-    }
+    }])
   ];
 
   title = 'formly';
